663dba841496e0eee1f1c33fb2f80c84008c9750
Sajid18May <sajid1852002@gmail.com>
2023-08-31 15:00:41 +0530

    2dArray

---
+++ src/TwoDArray.java
@@ -0,0 +1,15 @@
+
+public class TwoDArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int [][]a= {{2,5,6,8},{78,89,85,56},{89,5,6,2}};
+for(int i=0;i<3;i++) {
+	for(int j=0;j<4;j++) {
+		System.out.print(a[i][j]+" ");
+	}
+	System.out.println(" ");
+}
+	}
+
+}
