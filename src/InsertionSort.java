252355f0f0b0f8367f228425495147160a5ab633
Sajid18May <sajid1852002@gmail.com>
2025-02-19 01:12:39 +0530

    Day10

---
+++ src/InsertionSort.java
@@ -0,0 +1,20 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr={6,8,2,4,9,12,46,7};
+        insertionSort(arr);
+        for (int i:arr) System.out.print(i+" ");
+    }
+
+    private static void insertionSort(int[] arr) {
+        for (int i = 1; i <arr.length ; i++) {
+            int temp=arr[i];
+            int j=i-1;
+            while (j>=0 && arr[j]>temp ){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+
+        }
+    }
+}
