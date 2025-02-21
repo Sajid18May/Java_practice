01845b2a0178ced8b86d5ea1326a6c11809f4e11
Sajid18May <sajid1852002@gmail.com>
2025-02-21 11:29:25 +0530

    Day11

---
+++ src/UnaryOperator.java
@@ -0,0 +1,8 @@
+public class UnaryOperator {
+    public static void main(String[] args) {
+        int i=10;
+        int j=++i + i+ i++ + ++i;
+        System.out.println(i);//13
+        System.out.println(j);//46
+    }
+}
