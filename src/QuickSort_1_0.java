82041a557d698f1cce76756270345ba8d312a729
Sajid18May <sajid1852002@gmail.com>
2025-02-27 21:45:03 +0530

    day 14

--- src/QuickSort_1_0.java
+++ src/QuickSort_1_0.java
@@ -4,23 +4,28 @@ public class QuickSort_1_0 {
         quickSort(arr,0,arr.length-1);
         for (int i:arr) System.out.println(i);
     }
-    public static void quickSort(int[] arr,int low,int high){
+
+    private static void quickSort(int[] arr, int low, int high) {
         if(low>=high)return;
+
         int pivot=position(arr,low,high);
-        quickSort(arr,low,pivot-1);// 4 6 7 8 3 12 34 5 //4 3 (5) 7 8 6 12 34
+
+        quickSort(arr,low,pivot-1);
         quickSort(arr,pivot+1,high);
     }
-    public static int position(int []arr,int low,int high){
-        int i=low-1;// 4 6 7 8 3 12 34 (5)
+
+    private static int position(int[] arr, int low, int high) {
+        int i=low-1;
         int pivot=arr[high];
-        for (int j = low; j < high; j++) {
+        for(int j=low;j<high;j++){
             if(arr[j]<pivot){
                 i++;
-                arr[j]=arr[i]+arr[j]-(arr[i]=arr[j]);
+                arr[i]=arr[i]+arr[j]-(arr[j]=arr[i]);
             }
         }
         i++;
-        arr[high]=arr[i]+arr[high]-(arr[i]=arr[high]);
+        arr[i]=arr[i]+arr[high]-(arr[high]=arr[i]);
         return i;
     }
+
 }
