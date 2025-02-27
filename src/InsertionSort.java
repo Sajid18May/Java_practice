8718e08c81723cfe18ff2cbf8de045da78709b88
Sajid18May <sajid1852002@gmail.com>
2025-02-27 22:18:23 +0530

    day 14

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -6,15 +6,16 @@ public class InsertionSort {
     }
 
     private static void insertionSort(int[] arr) {
-        for (int i = 1; i <arr.length ; i++) {
-            int temp=arr[i];
+        for (int i = 1; i < arr.length ; i++) {
             int j=i-1;
-            while (j>=0 && arr[j]>temp ){
+            int temp=arr[i];
+            while (j>=0 && arr[j]>temp){
                 arr[j+1]=arr[j];
                 j--;
             }
             arr[j+1]=temp;
 
         }
+
     }
 }
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -10,7 +10,7 @@ public class SelectionSort {
     private static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int min=i;
-            for (int j = i; j < arr.length; j++) {
+            for (int j = i+1; j < arr.length; j++) {
                 if(arr[j]<arr[min]){
                     min=j;
                 }
