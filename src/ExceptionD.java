d6908ce127bf635fe4b4fcf8475972df07faa7fd
Sajid18May <sajid1852002@gmail.com>
2025-03-07 16:40:01 +0530

    day 17

---
+++ src/ExceptionD.java
@@ -0,0 +1,24 @@
+import java.io.FileReader;
+
+public class ExceptionD {
+    public static void main(String[] args) throws Exception {
+        ExceptionD e=new ExceptionD();
+        e.test1();
+        System.out.println(1);
+    }
+    void test1() throws Exception{
+        ExceptionD e=new ExceptionD();
+        e.test2();
+        System.out.println(2);
+    }
+    void test2() throws Exception {
+        ExceptionD e=new ExceptionD();
+        e.test3();
+        System.out.println(3);
+    }
+    void test3() throws Exception{
+        FileReader fr=new FileReader("D:\\logo\\text.txt");
+        fr.close();
+        System.out.println(4);
+    }
+}
