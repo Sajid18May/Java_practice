1e8221219ac23594d9d642378d5cdf0a96fe6e4e
Sajid18May <sajid1852002@gmail.com>
2025-04-25 03:13:55 +0530

    day 37

--- src/MedianOf2SortedArrays.java
+++ src/MedianOf2SortedArrays.java
@@ -7,35 +7,29 @@ public class MedianOf2SortedArrays {
         int m = nums1.length;
         int n = nums2.length;
 
-        if (m > n) return findMedianSortedArrays(nums2, nums1);
+        if(m>n) return findMedianSortedArrays(nums2,nums1);
 
-        int total = m + n;
-        int half = (total + 1) / 2;
+        int total=m+n;
+        int half=(total+1)/2;
 
-        int low = 0, high = m;
+        int low=0,high=m;
+        while (low<=high){
+            int mid1=low+(high-low)/2;
+            int mid2=half-mid1;
 
-        while (low <= high) {
-            int mid1 = (low + high) / 2;
-            int mid2 = half - mid1;
-
-            int l1 = (mid1 > 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
-            int l2 = (mid2 > 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
-            int r1 = (mid1 < m) ? nums1[mid1] : Integer.MAX_VALUE;
-            int r2 = (mid2 < n) ? nums2[mid2] : Integer.MAX_VALUE;
-
-            if (l1 <= r2 && l2 <= r1) {
-                if (total % 2 == 1) {
-                    return Math.max(l1, l2);
-                } else {
-                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
-                }
+            int l1=(mid1>0)?nums1[mid1-1]:Integer.MIN_VALUE;
+            int l2=(mid2>0)?nums2[mid2-1]:Integer.MIN_VALUE;
+            int r1=(mid1<m)?nums1[mid1]:Integer.MAX_VALUE;
+            int r2=(mid2<n)?nums2[mid2]:Integer.MAX_VALUE;
+            if(l1 <=r2 && l2<= r1){
+                if(total%2==1) return Math.max(l1,l2);
+                else return ((Math.max(l1,l2)+Math.min(r1,r2))/2.0);
             } else if (l1 > r2) {
-                high = mid1 - 1;
-            } else {
-                low = mid1 + 1;
+                high=mid1-1;
+            }else {
+                low=mid1+1;
             }
         }
-
         return 0.0;
     }
 }
