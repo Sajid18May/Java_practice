3896c966f05aa13fc36b72b02c7d1845fd238200
Sajid18May <sajid1852002@gmail.com>
2025-05-02 22:38:28 +0530

    day 42

---
+++ src/LargestOddInString.java
@@ -0,0 +1,11 @@
+public class LargestOddInString {
+    public static void main(String[] args) {
+        System.out.println(largestOddNumber("52"));
+    }
+    public static String largestOddNumber(String num) {
+        for(int i=num.length()-1;i>=0;i--){
+            if(num.charAt(i)%2==1) return num.substring(0,i+1);
+        }
+        return "";
+    }
+}
