4cbd51e1f1d290aadc1970736b36fcd71a11a472
Sajid18May <sajid1852002@gmail.com>
2025-05-08 21:11:39 +0530

    day 43+++

---
+++ src/RotateString.java
@@ -0,0 +1,12 @@
+public class RotateString {
+    public static void main(String[] args) {
+        String s = "abcde", goal = "cdeab";
+        System.out.println(rotateString(s,goal));
+    }
+    public static boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length()) return false;
+
+        s=s+s;
+        return s.contains(goal);
+    }
+}
