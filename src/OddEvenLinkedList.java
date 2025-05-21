74fef36edd2166455bf0729c180ca6cbe238290c
Sajid18May <sajid1852002@gmail.com>
2025-05-21 23:17:26 +0530

    day 50

---
+++ src/OddEvenLinkedList.java
@@ -0,0 +1,17 @@
+
+public class OddEvenLinkedList {
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode odd=head;
+        ListNode evenHead=head.next;
+        ListNode even=head.next;
+        while(even!=null && even.next!=null){
+            odd.next=odd.next.next;
+            even.next=even.next.next;
+            odd=odd.next;
+            even=even.next;
+        }
+        odd.next=evenHead;
+        return head;
+    }
+}
