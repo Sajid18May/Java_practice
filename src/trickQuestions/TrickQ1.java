4931cb9c044750c2938e8da6e2289a8fa4d3607e
Sajid18May <sajid1852002@gmail.com>
2025-08-03 02:23:09 +0530

    day 57++++

---
+++ src/trickQuestions/TrickQ1.java
@@ -0,0 +1,14 @@
+package trickQuestions;
+
+public class TrickQ1 {
+    public static void main(String[] args) {
+        Integer a=100;
+        Integer b=100;
+
+        Integer x=200;
+        Integer y=200;
+
+        System.out.println(a==b);
+        System.out.println(x==y);
+    }
+}
