2fc7dc20cf241f3d5aa12ec08d8e07bfd7a583a5
Sajid18May <sajid1852002@gmail.com>
2025-10-05 00:37:20 +0530

    SteamApi4

--- src/StreamApi/problemX.java
+++ src/StreamApi/problemX.java
@@ -1,4 +1,21 @@
 package StreamApi;
 
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+
 public class problemX {
+    static void main() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        IntSummaryStatistics stats = numbers.stream()
+                .mapToInt(Integer::intValue)
+                .summaryStatistics();
+
+        System.out.println("Max: " + stats.getMax());
+        System.out.println("Min: " + stats.getMin());
+        System.out.println("Average: " + stats.getAverage());
+        System.out.println("Sum: " + stats.getSum());
+        System.out.println("Count: " + stats.getCount());
+    }
 }
