257707b551fb4fc2e7c22b70e730b0e0370a42a8
Sajid18May <sajid1852002@gmail.com>
2025-10-16 00:02:48 +0530

    IsPalindrome

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,14 +1,28 @@
 public class PalindromeChecker {
     static void main() {
-        String text="SASA";
-        System.out.println(checkPalindrome(text));
+        String text = "SASA";
+        int number = 212;
+        System.out.println(checkPalindromeString(text));
+        System.out.println(checkPalindromeInteger(number));
     }
 
-    private static String checkPalindrome(String text) {
+    private static String checkPalindromeString(String text) {
         for (int i = 0; i < text.length(); i++) {
             if(text.charAt(i) != text.charAt(text.length()-1-i))
                 return text + " is not Plaindrome";
         }
         return text + " is Palindrome";
+    }
+    private static String checkPalindromeInteger(int number){
+        int rev=0,og=number;
+        while(number>0) {
+            int dig=number%10;
+            rev=rev*10+dig;
+            number/=10;
+        }
+        if(og!=rev)
+            return og + " is not Palindrome";
+        else
+            return og + " is Palindrome";
     }
 }
