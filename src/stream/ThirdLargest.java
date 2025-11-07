8e620db940eed992b56297657ae77fde1d6bef7e
Sajid18May <sajid1852002@gmail.com>
2025-11-07 10:36:52 +0530

    ThirdLargest

--- src/stream/ThirdLargest.java
+++ src/stream/ThirdLargest.java
@@ -1,17 +1,18 @@
 package stream;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class ThirdLargest {
     static void main() {
         List<Integer> list = Arrays.asList(10, 5, 20, 20, 15, 30, 15, 25);
 
-        int max = list.stream()
-                .sorted()
-                .skip(2)
-                .limit(1)
-                .getClass().getModifiers();
-        System.out.println(max);
+        Optional<Integer> max = list.stream()
+                .sorted(Comparator.reverseOrder())
+                .skip(2) // 3. Skip the 1st and 2nd largest (index 0 and 1)
+                .findFirst();
+        System.out.println(max.get());
     }
 }
